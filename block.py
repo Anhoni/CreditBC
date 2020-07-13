c24089a68de606054db447eb0309be74bdb704e9
Soni <ankit.c.soni@capgemini.com>
2020-07-13 13:30:00 +0530

    working prototype
    Signed-off-by: Soni <ankit.c.soni@capgemini.com>

--- block.py
+++ block.py
@@ -4,11 +4,21 @@ from creditscore import score
 class block:
     """this class define block that will be created by both transaction and creditscore"""
     def __init__(self):
-        self.previous = "101"
-        self.transaction = [trans()]
+        __tTranRead = open("transactions.txt", "r")
+        __allTrans = __tTranRead.readlines()
+        __tTranRead.close()
+        self.previous = __allTrans
+        __newID = __allTrans[len(__allTrans) - 1]
+        __newID = int(__newID.partition("\t")[0]) + 1
+        __currentTransaction = trans(__newID)
+        self.transaction = __currentTransaction
         __str = [self.previous, self.transaction]
         self.blockhash = hash(str(__str))
-
+        __tTranWrite = open("transactions.txt", "a+")
+        __tTranWrite.write("\n" + __currentTransaction.returnTransaction())
+        __tTranWrite.close()
+        __blockWrite = open("Blocks.txt","a")
+        __blockWrite.write("\n" + str(self.blockhash))
     def Blockhash(self):
         return  self.blockhash
 
--- transaction.py
+++ transaction.py
@@ -1,10 +1,13 @@
 class trans:
     """this is block of transaction"""
-    def __init__(self):
-        self.tid = input("transaction ID : ")
-        self.tamount = input("Enter amount: ")
+    def __init__(self, tid):
+        self.tid = tid
         self.tsender = input("Enter sender: ")
         self.treciver = input("Enter recipient: ")
+        self.tamount = input("Enter amount: ")
+
+    def returnTransaction(self):
+        return str(self.tid) +"\t" + self.treciver +"\t" + self.tsender +"\t" + self.tamount
 
 
 
